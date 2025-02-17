a032634498bce4674db7abae1ec8e4e8e568ef8d
PC Steven <steventon00@gmail.com>
2025-02-17 16:18:56 -0500

    Doc:
    added main file

---
+++ main.py
@@ -0,0 +1,16 @@
+#imports
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
+
+
+# Scraps data from available data provided
+def start_script():
+     print("Scraping data...")
+     wikiUrl = "https://dokkaninfo.com/cards"
+
+if __name__ == "__main__":
+    start_script()
